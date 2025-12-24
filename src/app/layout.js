1386266415445570e617450237c40737b9958e47
Abhishek Nigam <113419871+AbhishekSpotOp@users.noreply.github.com>
2025-12-25 02:17:44 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,25 +3,16 @@ import MusicButton from "@/components/MusicButton";
 
 export const metadata = {
   title: "Happy Birthday!",
-  description: "An animated birthday surprise filled with emotions, words from the heart, and a letter that types itself — just for you."
+  description:
+    "An animated birthday surprise filled with emotions, words from the heart, and a letter that types itself — just for you.",
 };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-        <MusicButton />
-      </body>
-    </html>
-  );
-}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`antialiased`}
-      >
+      <body className="antialiased">
         {children}
+        <MusicButton />
       </body>
     </html>
   );
